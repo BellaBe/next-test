448b88d01583a9190ef16aae58cbb430f5e28e3f
BellaBelgarokova <bella.belgarokova@businesspartner.roche.com>
2023-08-14 10:30:27 +0100

    add cms and fetch data from it

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -1,6 +1,9 @@
 import { readdir, readFile } from "node:fs/promises";
 import matter from "gray-matter";
 import { marked } from "marked";
+import qs from "qs"
+
+const CMS_URL = "http://localhost:1337"
 
 export async function getFeaturedReview() {
     const reviews = await getReviews()
@@ -8,26 +11,57 @@ export async function getFeaturedReview() {
 }
 
 export async function getReview(slug) {
-    const text = await readFile(`./content/reviews/${slug}.md`, "utf-8")
-    const { content, data: { title, date, image } } = matter(text);
-    const body = marked(content)
-    return { slug, title, date, image, body }
+    const { data } = await fetchReviews(
+        {
+            filters: { slug: { $eq: slug } },
+            fields: ["slug", "title", "subtitle", "publishedAt", "body"],
+            populate: { image: { fields: ["url"] } },
+            pagination: { pageSize: 1, withCount: false }
+        }
+    )
+    const { attributes: { title, publishedAt, body, image } } = data[0]
+
+    return {
+        slug,
+        title,
+        date: publishedAt.slice(0, 0, "yyyy-mm-dd".length),
+        image: CMS_URL + image.data.attributes.url,
+        body: marked(body)
+    }
 }
 
 export async function getReviews() {
-    const slugs = await getSlugs()
-    const reviews = []
-    for (const slug of slugs){
-        const review = await getReview(slug)
-        reviews.push(review)
-    }
-    const sortedReviews = reviews.sort((a, b) => b.date.localeCompare(a.date))
 
-    return sortedReviews
+    const { data } = await fetchReviews({
+        fields: ["slug", "title", "subtitle", "publishedAt"],
+        populate: { image: { fields: ["url"] } },
+        sort: ["publishedAt:desc"],
+        pagination: { pageSize: 6 }
+    })
+
+    return data.map(({ attributes }) => ({
+        slug: attributes.slug,
+        title: attributes.title,
+        date: attributes.publishedAt.slice(0, "yyyy-mm-dd".length),
+        image: CMS_URL + attributes.image.data.attributes.url
+    }))
 }
 
 export async function getSlugs() {
-    const files = await readdir("./content/reviews")
-    const slugs = files.filter(file => file.endsWith(".md")).map(file => file.slice(0, -".md".length))
-    return slugs
+    const { data } = await fetchReviews({
+        fields: ["slug"],
+        sort: ["publishedAt:desc"],
+        pagination: { pageSize: 100 }
+    })
+    return data.map(item => item.attributes.slug)
+}
+
+async function fetchReviews(parameters) {
+    const url = `${CMS_URL}/api/reviews?`
+        + qs.stringify(parameters, { encodeValuesOnly: true })
+    const response = await fetch(url)
+    if(!response.ok){
+        throw new Error(`CMS returned ${response.status} for ${url}`)
+    }
+    return await response.json()
 }
---
+++ scripts/strapi-request.mjs
@@ -0,0 +1,15 @@
+import { writeFileSync } from "node:fs"
+import qs from "qs"
+const url = "http://localhost:1337/api/reviews"
+    + "?" + qs.stringify({
+        fields: ["slug", "title", "subtitle", "publishedAt"],
+        populate: {image: {fields: ["url"]}},
+        sort: ["publishedAt:desc"],
+        pagination: {pageSize: 6}
+    }, { encodeValuesOnly: true })
+console.log(url)
+const response = await fetch(url)
+const body = await response.json()
+const file = "scripts/strapi-response.json"
+console.log(JSON.stringify(body, null, 2))
+writeFileSync(file, JSON.stringify(body, null, 2), "utf-8")
